cdc89224aabd1df1d8a68e10efd99b8b2c961c88
Joyqul Chen <joy1357924680@gmail.com>
2014-03-19 09:53:52 +0800

    Do sign up, not crypt yet.

--- login.py
+++ login.py
@@ -32,24 +32,56 @@ def do_signin(session):
 
 def check_signin(user_email, passwd):
     cursor.execute("select `password` from `user` where `account`= %s", (user_email)) 
-    correct_passwd = (cursor.fetchall())[0][0]
-    if correct_passwd != "" and passwd == correct_passwd:
-        return True
+    data = cursor.fetchall()
+    if data == ():
+        return template('signup', title="Sign Up For Flight Time Table", 
+                warning="No such user")
+
     else:
-        return False
+        correct_passwd = data[0][0]
+        if correct_passwd != None and passwd == correct_passwd:
+            return True
+        else:
+            return False
 
 
 @route('/flight/signup')
-def server_static():
-    return template('signup', title="Sign Up For Flight Time Table")
+def server_static(session):
+    return template('signup', title="Sign Up For Flight Time Table", warning="")
+
+@route('/flight/signup', method = 'POST')
+def do_signup(session):
+    user_email = request.forms.get('email')
+    passwd = request.forms.get('password')
+    passwd_conf = request.forms.get('password_confirm')
+    is_admin = request.forms.get('is_admin')
+
+
+    if passwd != passwd_conf:
+        return template('signup', title = "Sign Up For Flight Time Table", 
+                warning = "Password Confirmation Failed.")
+
+    cursor.execute('select * from `user` where `account` = %s', (user_email))
+
+    if cursor.fetchall() != ():
+        return template('signup', title = "Sign Up For Flight Time Table",
+                warning = "Email is repeate.")
+    else:
+        if is_admin == 'on':
+            is_admin = True
+        else:
+            is_admin = False
+        cursor.execute('insert into `user` values(0, %s, %s, %s)', (user_email, passwd, is_admin))
+        session['is_admin'] = is_admin
+        redirect('/database/flight/timetable')
 
 @route('/flight/timetable')
 def index(session):
     is_admin = session.get('is_admin')
-    if is_admin == True:
+    if is_admin == 'True' or is_admin == True:
         return "Hello", is_admin
     else:
-        return "QQ"
+        return "QQ", is_admin
 
 @bottle.route('/flight/set/<user_name>')
 def set_name(session, user_name=None):
--- views/signup.tpl
+++ views/signup.tpl
@@ -6,20 +6,21 @@
     <link rel="stylesheet" type="text/css" href="../static/style.css" />
 </head>
 <body>
+    <p> {{warning}} </p>
     <h1> Sign up</h1>
-    <form action="/login" method="post">
+    <form action="/database/flight/signup" method="post">
         Email </br>
         <input name="email" type="text" />
         </br>
         Password </br>
-        <input value="Sign in" type="password" />
+        <input name="password" type="password" />
         </br>
         Password confirmation</br>
-        <input value="Sign in" type="password" />
+        <input name="password_confirm" type="password" />
         </br>
-    </form>
-    <p><input type="checkbox"> Admin User </br></p>
+    <p><input name="is_admin" type="checkbox"> Admin User </br></p>
     <p><input type="submit" value="Sign up"></br></p>
+    </form>
     <p><a href="signin">Sign in</a></p>
 </body>
 </html>
