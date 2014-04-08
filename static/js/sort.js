2d99b41162958ec00dad69af3d8a0129373cc943
Joyqul Chen <joy1357924680@gmail.com>
2014-04-08 23:27:47 +0800

    Use tablesorter in comparison sheet.

---
+++ static/js/sort.js
@@ -0,0 +1,18 @@
+$(function() {
+  $("#flight").tablesorter(); 
+});
+
+$(document).ready(function() {
+  $("#flight").tablesorter({ 
+    sortList: [[0,0], [1,0]],
+    0: { sorter: "digit" },
+    1: { sorter: "text" },
+    2: { sorter: "text" },
+    3: { sorter: "text" },
+    4: { sorter: "text" },
+    5: { sorter: "text" },
+    6: { sorter: "digit" },
+    7: { sorter: false },
+    8: { sorter: false },
+  }); 
+});
